c9487326abc0e51c0bb15b64ee8c54bb3a841dfb
James <jpaik456@gmail.com>
2017-05-24 17:49:07 +0000

    Changes to socket and scss

--- socketApi.js
+++ socketApi.js
@@ -1,11 +1,35 @@
+var uuid = require('uuid/v4');
 var socketio = require('socket.io');
 var io = socketio();
 var socketApi = {};
+var util = require('util');
 
 socketApi.io = io;
+rooms = {}
 
 io.on('connection', function(socket) {
-    console.log('New user connected.');
+    var id = uuid();
+    socket.clientId = id;
+
+    // Lobby events
+    socket.on('join', function() {
+    });
+    socket.on('leave', function() {
+    });
+    socket.on('teamswitch', function(data) {
+    });
+    socket.on('roleswitch', function(data) {
+    });
+    socket.on('ready', function() {
+    });
+
+    // Game events
+    socket.on('hint', function(data) {
+    });
+    socket.on('chooseword', function(data) {
+    });
+    socket.on('votepass', function() {
+    });
 });
 
 module.exports = socketApi;
