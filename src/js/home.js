eb57bd32eb4daea1392dd6590bb699d5f6f32c81
James <jpaik456@gmail.com>
2017-05-26 01:35:35 +0000

    Add temporary ajax function for backend

--- src/js/home.js
+++ src/js/home.js
@@ -12,7 +12,7 @@ $(function() {
       confirmButtonClass: 'btn btn-default',
       cancelButtonClass: 'btn btn-default',
       buttonsStyling: false,
-      inputValidator: function(value) {
+      inputValidator: function(value) { //Checks if name is not null.
         return new Promise(function(resolve, reject) {
           if (value) {
             resolve();
@@ -22,33 +22,38 @@ $(function() {
         });
       },
       preConfirm: function(name) {
-        return new Promise(function(resolve, reject) {
-          setTimeout(function() {
-            if (name === 'false') {
-              reject('Game room full')
-            } else {
-              resolve()
+        return new Promise(function(resolve, reject) { //Returns a promise object, rejects if error and resolves if created.
+          $.ajax({
+            type: 'POST',
+            url: '/game/create',
+            data: {
+              name: name
             }
-          }, 2000)
+          }).done(function(data) {
+            Cookie.set('game_uuid', data.uuid, {expires: 1}); //Stores Session Unique ID
+            Cookie.set('game_code', data.code, {expires: 1}); //Stores game's code for resjoining
+            resolve([name, data.code]); //Returns name and code to redirect
+          }).fail(function() {
+            reject('Something went wrong... Try again.');
+          });
         })
       },
-    }).then(function(name) {
+    }).then(function(data) { //data = name and code
       swal({
         type: 'success',
-        html: 'Creating Room for ' + name
+        html: 'Creating Room for ' + data[0] + " | Code: " + data[1]
       });
       //window.location.href = /game/ + code
     }).catch(swal.noop);
-  }); //Create game click
+  }); //End Create game click
 
   //Join Existing Game
   $('.btn.join').on('click', function(e) {
-
     e.preventDefault();
     swal({
       title: 'Join a Game',
       html: '<input id="swal-input_name" class="swal2-input" placeholder="Your Name">' +
-        '<input id="swal-input_code" class="swal2-input" placeholder="Game Code">',
+        '<input id="swal-input_code" class="swal2-input" placeholder="Game Code" maxlength="4">',
       confirmButtonText: "Join",
       showCancelButton: true,
       showLoaderOnConfirm: true,
@@ -59,21 +64,38 @@ $(function() {
         $('#swal-input_name').focus();
       },
       preConfirm: function() {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function(resolve, reject) { //Returns a promise object, rejects if error and resolves if game exists.
           if ($('#swal-input_name').val() === "") {
             $('#swal-input_name').focus();
             reject('Please enter your name.');
+            return;
           }
-          else if ($('#swal-input_code').val() === "") {
+          if ($('#swal-input_code').val() === "") {
             $('#swal-input_code').focus();
             reject('Please enter game code.');
+            return;
           }
-          else{
+          $.ajax({
+            type: 'POST',
+            url: '/game/join',
+            data: {
+              name: $('#swal-input_name').val(),
+              code: $('#swal-input_code').val().toLowerCase()
+            }
+          }).done(function(data, jqXHR) {
+            Cookie.set('game_code', data.code, {expires: 1});
+            Cookie.set('game_uuid', data.uuid, {expires: 1});
             resolve([
               $('#swal-input_name').val(),
               $('#swal-input_code').val()
             ]);
-          }
+          }).fail(function(jqXHR) {
+            if(jqXHR.status === 400){
+              reject('Game not found.');
+            }else{
+              reject('Something went wrong... Try again.');
+            }
+          });
         })
       }
     }).then(function(result) {
@@ -84,7 +106,6 @@ $(function() {
       //window.location.href = /game/ + code
     }).catch(swal.noop);
 
-  }); //join button click
-
+  }); //End join button click
 
 }); //Ends $function
