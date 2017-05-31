7a5656868c8332d4bddc071b5509af52117c4c0c
James <jpaik456@gmail.com>
2017-05-31 22:05:07 +0000

    Add Role switching. Finish basic lobby functions.

--- src/js/game.js
+++ src/js/game.js
@@ -19,19 +19,32 @@ $(function() {
   Lobby View
   ====================
   */
+  $('view.lobby span.gameCode').text(Cookies.get('gameCode').toUpperCase()); //Shows game's code
   //Client Events
   let currentTeam = "",
     currentRole = "player",
     currentReady = false,
     currentUserId = Cookies.get('userId');
   let userView = $('view.lobby .users');
-  $('view.lobby').on('click tap', '.users h1', function() { //Changes teams
+  $('view.lobby').on('click tap', '.users h1', function(e) { //Changes teams
       let team = $(this).parent().data('team'); //Gets teams name
       if (!team || currentTeam === team) return; //If team doesn't exist or they're on the team already, then don't do anything.
       currentTeam = team; //Change team to new team.
+      currentRole = "player"; //Resets user to player role.
       joinTeam();
+      e.preventDefault();
     })
-    .on('click tap', '.buttons .btn', function() {
+    .on('click tap', 'ul.master > li .btn', function(e) {
+      let role = $(this).data('role');
+      if (role === "master") { //Player wants to join as master
+        currentRole = "master";
+      } else { //Master wants to switch to player
+        currentRole = "player";
+      }
+      switchRole();
+      e.preventDefault();
+    })
+    .on('click tap', '.buttons .btn', function(e) {
       let type = $(this).data('type');
       if (type === "ready") {
         if (currentReady) { //User clicked Unready
@@ -47,6 +60,7 @@ $(function() {
       } else if (type === "leave") {
         socket.emit('leave');
       }
+      e.preventDefault();
     });
 
 
@@ -56,7 +70,8 @@ $(function() {
       teamName: currentTeam
     });
   }
-  function currentUserSwitchTeam(){ //Switches user to team on confirm
+
+  function currentUserSwitchTeam() { //Switches user to team on confirm
     $(userView).find('[data-id="' + currentUserId + '"]').remove(); //Delete from list
     let user = createUser(currentUserId, Cookies.get('name'));
     $(user).appendTo($(userView).find('.' + currentTeam + " ul.player"));
@@ -64,24 +79,27 @@ $(function() {
     $(userView).find('h1 .pull-right').show(); //Show the join icon
     $(userView).find('.' + currentTeam + " h1 .pull-right").hide(); //Hide this team's join icon
 
+    checkSpymasters();
     updateThemeColor(currentTeam);
-    if(currentReady) $('view.lobby .btn[data-type="ready"]').click(); //Unready the user if they were ready on team switch.
+    if (currentReady) $('view.lobby .btn[data-type="ready"]').click(); //Unready the user if they were ready on team switch.
   }
-  function switchRole(){ //handles user switching role
-    socket.emit('teamswitch', {
+
+  function switchRole() { //handles user switching role
+    socket.emit('roleswitch', {
       roleName: currentRole
     });
   }
-  function currentUserSwitchRole(){
+
+  function currentUserSwitchRole() {
     $(userView).find('[data-id="' + currentUserId + '"]').remove(); //Delete from list
-    let user = createUser(currentUserId, Cookies.get('name'));
-    $(user).appendTo($(userView).find('.' + currentTeam + " ul.player"));
-
-    $(userView).find('h1 .pull-right').show(); //Show the join icon
-    $(userView).find('.' + currentTeam + " h1 .pull-right").hide(); //Hide this team's join icon
-
-    updateThemeColor(currentTeam);
-    if(currentReady) $('view.lobby .btn[data-type="ready"]').click(); //Unready the user if they were ready on team switch.
+    let user = createUser(currentUserId, Cookies.get('name'), currentRole);
+    $(user).appendTo($(userView).find('.' + currentTeam + " ul." + currentRole));
+    checkSpymasters();
+    if (currentRole === "master") {
+      $(userView).find('[data-id="' + currentUserId + '"]').append('<span class="spymaster"><a data-role="player" class="btn btn-default" href="#">Become Player</a></span>')
+    }
+    console.log("Current Role: " + currentRole);
+    if (currentReady) $('view.lobby .btn[data-type="ready"]').click(); //Unready the user if they were ready on team switch.
   }
 
   function playerSwitchedTeam(id, team) { //handles other user switching team. team = red/blue
@@ -90,22 +108,29 @@ $(function() {
     $(user).remove();
     let switchedUser = createUser(id, uname);
     $(switchedUser).appendTo($(userView).find('.' + team + " ul.player"));
+    checkSpymasters();
   }
+
   function playerSwitchedRoles(id, role) { //handles other user switching role. role=player/master
     let user = $(userView).find('[data-id="' + id + '"]');
     let uname = $(user).data('name'),
       team = $(user).closest('.team').data('team');
     $(user).remove();
-    let switchedUser = createUser(id, uname);
+    let switchedUser = createUser(id, uname, role);
     $(switchedUser).appendTo($(userView).find('.' + team + " ul." + role));
+    checkSpymasters();
   }
+
   function playerJoined(id, name, team) { //handles user joining. name = username, team = red/blue
     $(createUser(id, name)).appendTo($(userView).find('.' + team + " ul.player"));
   }
+
   function playerLeft(id) { //handles user  leaving
     let switchedUser = $(userView).find('[data-id="' + id + '"]');
     $(userView).find('[data-id="' + id + '"]').remove();
+    checkSpymasters();
   }
+
   function playerToggleReady(id, isReady) { //handles any user ready/not ready. isReady = boolean
     let user = $(userView).find('[data-id="' + id + '"]');
     if (isReady) {
@@ -114,11 +139,30 @@ $(function() {
       $(user).find('.toggleReady').removeClass('fa-check-square-o').addClass('fa-square-o');
     }
   }
-  var createUser = function(id, name, role="player", ready=false) { //role = player unless it's a master, ready = false unless they're ready
+
+  function checkSpymasters() { //Checks if there are spymasters on the team. If not, then shows placeholder.
+    var noMasterHtml = `
+      <li class="nomaster">
+        <i class="fa fa-user-secret"></i>&nbsp; No Spymaster
+        <span class="collapse spymaster"><a data-role="master" class="btn btn-default" href="#">Become Spymaster</a></span>
+      </li>
+    `;
+    $(userView).find('ul.master li.nomaster').remove();
+    ['red', 'blue'].forEach(function(t) {
+      // console.log("Checking masters: " + t);
+      if ($(userView).find('.' + t + " ul.master li:not('.nomaster')").length < 1) {
+        $(noMasterHtml).appendTo($(userView).find('.' + t + " ul.master"));
+      }
+    });
+    $(userView).find('ul.master .spymaster').hide(); //Hide other team's join spymater button
+    $(userView).find('.' + currentTeam + " ul.master .spymaster").show(); //Show this team's join spymaster button
+  }
+
+  var createUser = function(id, name, role = "player", ready = false) { //role = player unless it's a master, ready = false unless they're ready
     let html = `
       <li data-id="${id}" data-name="${name}">
         <span class="pull-right"><i class="toggleReady fa ${ready ? "fa-check-square-o" : "fa-square-o"}"></i></span>
-        <i class="fa ${role === "player" ? "fa-user-o" : "fa-user-secret"}"></i>&nbsp; ${name}
+        <i class="fa ${role === "player" ? "fa-user-o" : "fa-user-secret"}"></i>&nbsp; ${role === "master" ? "Spymaster:" : ""}&nbsp;${name}
       </li>
       `;
     return html;
@@ -139,6 +183,7 @@ $(function() {
         initUsers(data.users); //Appends all existing users
         currentTeam = data.teamName;
         joinTeam(); //Creates user
+        checkSpymasters(); //Checks Spymasters
       } else { // Mid game
         console.log("Mid Game?");
       }
@@ -155,17 +200,28 @@ $(function() {
       console.log("Confirm Team Switch");
       currentUserSwitchTeam();
     })
+    .on('failteamswitch', function() { //Failed Current User Team Switch
+      console.log("Failed Team Switch");
+      currentTeam = (currentTeam === "red" ? "blue" : "red");
+      currentUserSwitchTeam();
+    })
     .on('confirmroleswitch', function() { //Confirmed Current User Role Switch
       console.log("Confirm Role Switch");
       currentUserSwitchRole();
     })
-    .on('failready', function(){ //If the ready/unready request fails, then revert it.
-      console.log("Failed Ready");
-      if(currentReady) $('view.lobby .btn[data-type="ready"]').click();
+    .on('failroleswitch', function() { //Failed Current User Role Switch
+      console.log("Failed Role Switch");
+      currentRole = (currentRole === "player" ? "master" : "player");
+      currentUserSwitchRole();
+      checkSpymasters();
     })
-    .on('failunready', function(){ //If the ready/unready request fails, then revert it.
+    .on('failready', function() { //If the ready/unready request fails, then revert it.
+      console.log("Failed Ready");
+      if (currentReady) $('view.lobby .btn[data-type="ready"]').click();
+    })
+    .on('failunready', function() { //If the ready/unready request fails, then revert it.
       console.log("Failed Unrady");
-      if(!currentReady) $('view.lobby .btn[data-type="ready"]').click();
+      if (!currentReady) $('view.lobby .btn[data-type="ready"]').click();
     })
     .on('player_join', function(data) { //Another player joined.
       console.log('Player Joined Game');
